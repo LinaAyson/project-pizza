1456be3691284ff0b4826668272612f839a0d9f6
linaayson <andersson.lina89@gmail.com>
2023-09-28 15:15:42 +0200

    step 3

--- code/script.js
+++ code/script.js
@@ -25,13 +25,57 @@ if (foodOrder === "1") {
   selectedOrder = "Pasta"
 } else if (foodOrder === "3") {
   selectedOrder = "Salad"
+} else {
+  alert("Invalid choice, please try again!")
+  exit(1);
 }
-alert(`Good choice, one ${selectedOrder}`)
+alert(`You selected ${selectedOrder}`)
 
 
 // Step 3 - Subtype choice
 // Your code goes here
+let foodItem = "";
+let foodMenu = [];
 
+switch (selectedOrder) {
+
+  case "Pizza":
+    foodItem = prompt(`Choose your pizza
+    1 - Margarita 
+    2 - Vegeteriana
+    3 - Funghi 
+    Please enter the number of your choice`);
+    foodMenu = ["Margarita", "Vegeteriana", "Funghi"]
+    break;
+
+  case "Pasta":
+    foodItem = prompt(`Choose your pasta
+    1 - Pasta Vegeteriana
+    2 - Pasta w tomatosauce
+    3 - Lasagna`)
+    foodMenu = ["Pasta Vegeteriana", "Pasta w tomatosauce", "Lasagna"]
+    break;
+
+  case "Salad":
+    foodItem = prompt(`Choose your salad
+      1 - Tomatosalad
+      2 - Qournsalad
+      3 - Greeksalad
+      `)
+    foodMenu = ["Tomatosalad", "Qournsalad", "Greeksalad"]
+    break;
+  default:
+    ("Invalid chice, please try again!")
+    exit(1)
+};
+
+if (foodItem === "1") {
+  alert(`Good choice! one ${foodMenu[0]} is coming up`)
+} else if (foodItem === "2") {
+  alert(`Good choice, one ${foodMenu[1]} is coming up`)
+} else if (foodItem === "3") {
+  alert(`Good choice, one ${foodMenu[2]} is coming up`)
+}
 
 // Step 4 - Age
 // Your code goes here
