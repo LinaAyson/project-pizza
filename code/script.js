4a134b28571e841208bbe40de1e483e3410f0628
linaayson <andersson.lina89@gmail.com>
2023-09-28 14:30:03 +0200

    step 2

--- code/script.js
+++ code/script.js
@@ -5,13 +5,33 @@
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
+const guestName = prompt(`What is your name?`);
+alert(`Hello ${guestName} and welcome!`)
+
 
 // Step 2 - Food choice
-let guestName = prompt(`What is your name?`);
-alert(`Hello ${guestName}`)
+const foodOrder = prompt(
+  `What would you like to eat today?
+1 - Pizza
+2 - Pasta
+3 - Salad
+Please enter the number of your choice
+`
+)
+let selectedOrder = "";
+if (foodOrder === "1") {
+  selectedOrder = "Pizza"
+} else if (foodOrder === "2") {
+  selectedOrder = "Pasta"
+} else if (foodOrder === "3") {
+  selectedOrder = "Salad"
+}
+alert(`Good choice, one ${selectedOrder}`)
+
 
 // Step 3 - Subtype choice
 // Your code goes here
+
 
 // Step 4 - Age
 // Your code goes here
